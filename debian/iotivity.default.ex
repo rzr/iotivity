acfbc6aded2590938b177d6b8340423356980d63
Philippe Coval <philippe.coval@osg.samsung.com>
2016-09-06 10:11:13 +0200

    debian: Initial packaging on upstream git
    deb packages can be built from debian compatibles OS using:
    
       ./debian/rules
    
    For more details please check:
    https://wiki.iotivity.org/os
    
    Change-Id: I7e903d0419b39aefd1d47b57e51d0442109e7a85
    Bug-Debian: http://bugs.debian.org/824155
    Origin: https://github.com/tizenteam/iotivity
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ debian/iotivity.default.ex
@@ -0,0 +1,10 @@
+# Defaults for iotivity initscript
+# sourced by /etc/init.d/iotivity
+# installed at /etc/default/iotivity by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
